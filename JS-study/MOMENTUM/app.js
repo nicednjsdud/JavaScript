376fdab9f81ec0e572b3171d98092f603f90897c
nicednjsdud <nicednjsdud@naver.com>
2022-06-25 01:21:48 +0900

    자바스크립트 기초 공부

--- JS-study/MOMENTUM/app.js
+++ JS-study/MOMENTUM/app.js
@@ -30,3 +30,68 @@ console.log(daysOfWeek[5]); // sat
 daysOfWeek.push("week");
 
 console.log(daysOfWeek);
+
+// 2.6 강의 Object
+
+const playerName = "BOB";
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = "little bit";
+
+const player = {
+  name: "BOB",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player);
+
+// 2.7,8 강의 Functions part One TWO
+
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+
+plus(8, 60);
+
+function divide(a, b) {
+  console.log(a / b);
+}
+divide(98, 20);
+
+const palyer1 = {
+  name: "nico",
+  sayHello1: function (otherPersonsName) {
+    console.log("hellllo!! " + otherPersonsName + " nice to meet you ");
+  },
+};
+console.log(palyer1.name);
+palyer1.sayHello1("lynn");
+
+// 2.9 Recap
+
+let isNicoFat = true;
+isNicoFat = false;
+
+const me = "sexy";
+const days = [1, 2, false, true, null, undefined, "text"];
+const toBuy = ["potato", "tomato", "pizza"];
+
+console.log(toBuy);
+toBuy[2] = "water";
+console.log(toBuy);
+toBuy.push("one");
+console.log(toBuy);
